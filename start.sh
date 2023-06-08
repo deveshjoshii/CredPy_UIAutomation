1e6f6af007526bf7473195afc98cbdd766092f3f
Devesh Joshi <devesh.joshi@iquanti.com>
2023-06-08 16:30:29 +0530

    revised chanegs for parameter

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "start test case"
-browserstack-sdk pytest -s --endpoint=$1 testCase/test_Cpl.py
+browserstack-sdk pytest -s --end_point=$1 testCase/test_Cpl.py
 #"--end_point=${endpoint}"
