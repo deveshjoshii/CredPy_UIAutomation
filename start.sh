9cc180c04e7602a4e76140d3d1c1eb33cc6939d4
Devesh Joshi <devesh.joshi@iquanti.com>
2023-11-14 22:44:27 +0530

    allurereportchange

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "start test case on"
 
-browserstack-sdk pytest -s -m smoke --html=reports/report.html --end_point=$1 testCase
+browserstack-sdk pytest -s -m smoke --alluredir="./allureDir" --end_point=$1 testCase
 #"--end_point=${endpoint}"
